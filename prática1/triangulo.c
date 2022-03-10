e7eda2452a1c89e406a03ed3795e8563e9f690f7
JoaoMCV <99231431+JoaoMCV@users.noreply.github.com>
2022-03-09 23:49:19 -0300

    Add files via upload

---
+++ prática1/triangulo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int b;
+  int h;
+ 
+
+  printf("Qual a medida da base?\n");
+  scanf("%d", &b);
+  printf("Qual a altura do trinagulo?\n");
+  scanf("%d", &h);
+ double area = (b * h)/2;
+
+  printf("A área do triangulo é %lf", area);
+  
+  
+  return 0;
+}
