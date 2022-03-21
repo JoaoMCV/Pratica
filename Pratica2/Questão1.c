e3a072c4cea1318d3ec1e56c51a6a6d14b05499e
JoaoMCV <99231431+JoaoMCV@users.noreply.github.com>
2022-03-21 14:29:40 -0300

    Add files via upload

---
+++ Pratica2/Questão1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void){
+int n1,n2,n3,m;
+  printf("escreva os três numeros\n");
+  scanf("%i %i %i",&n1,&n2,&n3);
+  m = (n1 + n2 +n3)/3 ;
+    printf("a média é %i\n",m);
+}
