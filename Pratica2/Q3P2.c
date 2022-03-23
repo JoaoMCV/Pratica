5e3ce8d0950bc5560dc47a401e94790c25952bc5
JoaoMCV <99231431+JoaoMCV@users.noreply.github.com>
2022-03-23 20:00:25 -0300

    Add files via upload

---
+++ Pratica2/Q3P2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+  int temp,tempf;
+
+printf("escreva a temperatura em celcius:");
+scanf("%i",&temp);
+
+  tempf =((temp * 9)/5)+32 ;
+printf("%i",tempf);
+  
+}
